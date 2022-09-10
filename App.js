afc30f20b7614baffac33f6c351a09ee0b9bc3de
ExaCLe <biermann.leon@outlook.de>
2022-09-10 16:24:06 +0200

    firstVersion of the App

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ fontSize: 30 }}>Name: David Malen</Text>
+      <Text style={[styles.informationText, { color: "grey" }]}>
+        Phone Number: +01 06406364
+      </Text>
+
+      <Button title="Call" onPress={() => alert("Calling...")} />
     </View>
   );
 }
@@ -13,8 +16,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  informationText: {
+    fontSize: 20,
+    color: "red",
   },
 });
